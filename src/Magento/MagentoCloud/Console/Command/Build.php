a5339a6040ddd9e1964060e9c3b9ecd9192fa6d0
Rafael Gomes <rgomes@redstage.com>
2017-08-14 16:40:15 -0300

    Fix to implement in Magento 2.1.8

--- src/Magento/MagentoCloud/Console/Command/Build.php
+++ src/Magento/MagentoCloud/Console/Command/Build.php
@@ -56,8 +56,8 @@ class Build extends Command
     private function build()
     {
         $this->env->log("Start build.");
-        $this->applyMccPatches();
-        $this->applyCommittedPatches();
+//         $this->applyMccPatches();
+//         $this->applyCommittedPatches();
         $this->compileDI();
         $this->clearInitDir();
         $this->env->execute('rm -rf app/etc/env.php');
